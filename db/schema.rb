a14e6a071ea3232f4460bbfeb6345951267bb111
Martin Georgiev <thinkorswim1@abv.bg>
2014-02-16 23:31:29 +0200

    quick questions finished (for now)
    all pages up to date

--- db/schema.rb
+++ db/schema.rb
@@ -46,11 +46,6 @@ ActiveRecord::Schema.define(:version => 20140215211009) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
-  create_table "attaches", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "closed_group_posts", :force => true do |t|
     t.integer  "closed_group_id"
     t.text     "content"
@@ -120,13 +115,6 @@ ActiveRecord::Schema.define(:version => 20140215211009) do
   end
 
   create_table "likes_answer_questions", :force => true do |t|
-    t.integer  "quick_answer_id"
-    t.integer  "user_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-  end
-
-  create_table "likes_answers_questions", :force => true do |t|
     t.integer  "quick_answer_id"
     t.integer  "user_id"
     t.datetime "created_at",      :null => false
